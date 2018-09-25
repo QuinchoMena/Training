642ad6eeed37fc8aeb13f96e4f7a2d522aae54d8
jmena <jmena@akurey.com>
2018-09-25 13:41:02 -0600

    solve problem with render in mustache

--- RunForrest/src/js/main.js
+++ RunForrest/src/js/main.js
@@ -2,23 +2,27 @@ function validate(e) {
     if (e.keyCode===13) callPromise();
  }
 
-
  function callPromise(){
     fetch("https://api.myjson.com/bins/uptto")
     .then(Response => Response.json())
     .then(function(json) {
         var templateX = document.getElementById('template').innerHTML;
         var input = document.getElementsByName("inpuSearch")[0].value;
+ 
     if(input){
-        //hay datos
-        var output = Mustache.render(templateX, json.companies[0]===input);
+        console.log(json);
+        var filterCompanies = {}
+        filterCompanies.companies= json.companies.filter(function (el){
+            return el.name.toLowerCase().indexOf(input.toLowerCase()) > -1;
+        });
+        console.log(filterCompanies);
+        var output = Mustache.render(templateX,filterCompanies);
     }
     else{
         var output = Mustache.render(templateX, json);
     }
     document.getElementById('agents').innerHTML = output;
         
-
     }).catch(function (err){
         console.log(err);
     });
