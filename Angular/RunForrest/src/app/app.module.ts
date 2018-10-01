7f9ab3450afab1a89994e1a6345ca3912d6d7175
jmena <jmena@akurey.com>
2018-10-01 16:54:34 -0600

    add components

--- Angular/RunForrest/src/app/app.module.ts
+++ Angular/RunForrest/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyContentComponent } from './body-content/body-content.component';
+import { AgentsComponent } from './agents/agents.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SearchBarComponent,
+    HeaderComponent,
+    BodyContentComponent,
+    AgentsComponent
   ],
   imports: [
     BrowserModule,
